8b5d38af5d9cf6e2561ad7ce68c8a03f63c573f4
EzoVn <vincent.galpin@gmail.com>
2023-11-28 08:47:15 +0100

    Affichage du panier au clique sur l'icone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import "./navBar.css";
 
 const NavBar = () => {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  // const handleClickCart = () => {
+  //   setIsCartVisible(!isCartVisible);
+  // };
+
   return (
     <div className="navBar">
       <div className="navBar__left">
         <ul>
           <li>
-            <img
-              src="../../ecommerce-product-page-main/images/logo.svg"
-              alt="logo Sneakers"
-            />
+            <img src="./images/logo.svg" alt="logo Sneakers" />
           </li>
           <li>Collections</li>
           <li>Men</li>
@@ -19,16 +23,28 @@ const NavBar = () => {
         </ul>
       </div>
       <div className="navBar__right">
-        <img
-          className="icon-cart"
-          src="../../ecommerce-product-page-main/images/icon-cart.svg"
-          alt="Icon Cart"
-        />
-        <img
-          className="profil"
-          src="../../ecommerce-product-page-main/images/image-avatar.png"
-          alt="Photo de profil Avatar"
-        />
+        {/* <div className="cart" onClick={handleClickCart}> */}
+        <div className="cart" onClick={() => setIsCartVisible(!isCartVisible)}>
+          <img
+            className="icon-cart"
+            src="./images/icon-cart.svg"
+            alt="Icon Cart"
+          />
+          {isCartVisible && (
+            <div className="cart__visible">
+              <h2>Cart</h2>
+              {/*Article Panier */}
+            </div>
+          )}
+          <div className="hidden"></div>
+        </div>
+        <div>
+          <img
+            className="profil"
+            src="./images/image-avatar.png"
+            alt="Photo de profil Avatar"
+          />
+        </div>
       </div>
     </div>
   );
