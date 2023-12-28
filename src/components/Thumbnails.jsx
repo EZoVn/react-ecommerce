02c83faf26d7107eab62fbfdc409b877997db351
EzoVn <vincent.galpin@gmail.com>
2023-12-28 01:34:23 +0100

    approche differente pour afficher le carousel Au premier plan en tant que composant tjrs avec swiper js

--- src/components/Thumbnails.jsx
+++ src/components/Thumbnails.jsx
@@ -20,66 +20,99 @@ const productImagesThumbnails = [
   "../images/image-product-4-thumbnail.jpg",
 ];
 
+const ImagesProduct = ({
+  onThumbnailClick,
+  showThumbs,
+  handleClickToImage,
+  onSwiper,
+}) => {
+  // const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <>
+      {/* <div className="product__page-images"> */}
+      <div className={`product__page-images ${showThumbs ? "zoom" : ""}`}>
+        <Swiper
+          spaceBetween={10}
+          modules={Thumbs}
+          thumbs={{ swiper: "thumbs" }}
+          onSwiper={onSwiper}
+          className="main-image"
+        >
+          {productImages.slice(0).map((image, index) => (
+            <SwiperSlide key={index}>
+              <img
+                onClick={() => handleClickToImage(index)}
+                src={image}
+                alt={`Image produit ${index + 1}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          id="thumbs"
+          watchSlidesProgress={true}
+          slidesPerView={4}
+          spaceBetween={10}
+          modules={[Thumbs]}
+          className="thumbnail-container"
+        >
+          {productImagesThumbnails.slice(0).map((image, index) => (
+            <SwiperSlide key={index}>
+              <img
+                onClick={() => onThumbnailClick(index)}
+                className="thumbnail"
+                src={image}
+                alt={`Image produit thumbnail ${index + 1}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button
+          className={`closeBtn ${showThumbs ? "btnVisible" : ""}`}
+          // className="closeBtn"
+          onClick={handleClickToImage}
+        >
+          <img src="../images/icon-close.svg" alt="Close icon" />
+        </button>
+      </div>
+      {showThumbs && <div className="background-overlay"></div>}
+    </>
+  );
+};
+
 const ThumbnailCarousel = () => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [showThumbs, setShowThumbs] = useState(false);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const handleThumbnailClick = (index) => {
-    if (thumbsSwiper) {
-      thumbsSwiper.slideTo(index);
-    }
+    console.log("cliqué", index);
+    //   if (thumbsSwiper) {
+    thumbsSwiper.slideTo(index);
+    //   }
   };
   const handleClickToImage = (index) => {
     console.log("image principal", index);
     setShowThumbs(!showThumbs);
   };
   return (
-    <div className={`product__page-images ${showThumbs ? "zoom" : ""}`}>
-      <Swiper
-        spaceBetween={10}
-        modules={Thumbs}
-        thumbs={thumbsSwiper}
+    <>
+      <ImagesProduct
+        onThumbnailClick={handleThumbnailClick}
+        showThumbs={showThumbs}
+        handleClickToImage={handleClickToImage}
         onSwiper={(swiper) => setThumbsSwiper(swiper)}
-        className="main-image"
-      >
-        {productImages.slice(0).map((image, index) => (
-          <SwiperSlide key={index}>
-            <img
-              onClick={() => handleClickToImage(index)}
-              src={image}
-              alt={`Image produit ${index + 1}`}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <Swiper
-        id="thumbs"
-        watchSlidesProgress={true}
-        slidesPerView={4}
-        spaceBetween={10}
-        modules={[Thumbs]}
-        className="thumbnail-container"
-      >
-        {productImagesThumbnails.slice(0).map((image, index) => (
-          <SwiperSlide key={index}>
-            <img
-              onClick={() => handleThumbnailClick(index)}
-              className="thumbnail"
-              src={image}
-              alt={`Image produit thumbnail ${index + 1}`}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <button
-        className={`closeBtn ${showThumbs ? "btnVisible" : ""}`}
-        // className="closeBtn"
-        onClick={handleClickToImage}
-      >
-        <img src="../images/icon-close.svg" alt="Close icon" />
-      </button>
-      {showThumbs && <div className="background-overlay"></div>}
-    </div>
+      />
+      {showThumbs && (
+        <ImagesProduct
+          // className={`${showThumbs ? "zoom" : ""}`}
+          onThumbnailClick={handleThumbnailClick}
+          showThumbs={showThumbs}
+          handleClickToImage={handleClickToImage}
+          // onSwiper={(swiper) => thumbsSwiper(swiper)}
+        />
+      )}
+    </>
   );
 };
 
